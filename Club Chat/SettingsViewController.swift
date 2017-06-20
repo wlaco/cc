ca1e167a0769ed704186701a2fbd5860fc5b09bf
Will Laco <wlaco27@gmail.com>
2017-06-20 16:11:18 -0400

    First commit. settings vc put in with segues working

---
+++ Club Chat/SettingsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsViewController.swift
+//  Club Chat
+//
+//  Created by Will Laco on 6/20/17.
+//  Copyright © 2017 Will Laco. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func applyTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
